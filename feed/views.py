60f423c8b7778f24bd36b1e9338b421ff1ff3a8a
Ahmad Faraz <90551138+ahmadfaraz2@users.noreply.github.com>
2023-11-12 23:41:02 +0500

    now you can add post

--- feed/views.py
+++ feed/views.py
@@ -1,4 +1,9 @@
-from django.views.generic import TemplateView
+from typing import Any
+from django import http
+from django.views.generic import TemplateView, DetailView, FormView
+from django.contrib import messages
+
+from .forms import PostForm
 from .models import Post
 
 
@@ -8,5 +13,30 @@ class HomePageView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["posts"] = Post.objects.all()
+        context["posts"] = Post.objects.all().order_by("-id")
         return context
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "detail.html"
+
+
+class AddPostView(FormView):
+    form_class = PostForm
+    template_name = "new_post.html"
+    success_url = "/"
+
+    def dispatch(self, request, *args, **kwargs):
+        self.request = request
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        new_object = Post.objects.create(
+            text=form.cleaned_data["text"],
+            image=form.cleaned_data["image"],
+        )
+        messages.add_message(
+            self.request, messages.SUCCESS, "Your post was successfull."
+        )
+        return super().form_valid(form)
