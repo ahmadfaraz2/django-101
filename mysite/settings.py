f4f6d1f675ef6b3295dca5a0b40131d6854ca4ca
Ahmad Faraz <90551138+ahmadfaraz2@users.noreply.github.com>
2023-11-13 23:03:23 +0500

    Add static configurations in settings for learning

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-STATIC_ROOT = BASE_DIR / "staticfiles"  # os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = BASE_DIR / "staticfiles/"  # os.path.join(BASE_DIR, "staticfiles/")
 
 
 MEDIA_URL = "/media/"
